2f20ef0e707278bc341e2b546516be1e12368c2c
Armands Liepa <78017315+armands62@users.noreply.github.com>
2021-11-28 23:49:10 +0200

    Pabeigta programma

--- Programma.cpp
+++ Programma.cpp
@@ -3,10 +3,11 @@
 #include <time.h>
 #include <windows.h>
 #include <math.h>
+#include <conio.h>
 
 void matricu_saskaitisana() {
     system("CLS");
-    std::cout << "Matricu saskaitisana" << std::endl << std::endl;
+    std::cout << "MATRICU SASKAITISANA" << std::endl << std::endl;
     // Pirmās matricas ievade
     int MasivsA[3][3];
     for (int i = 0; i < 3; i++)
@@ -60,23 +61,34 @@ void matricu_saskaitisana() {
                 pareizs += 1;
             }
             std::cout << std::endl;
-            std::cout << std::endl;
         }
     }
     if (pareizs == 9)
     {
-        std::cout << "Pareizi!";
+        std::cout << "Pareizi!" << std::endl;
     }
     else
     {
-        std::cout << "Nepareizi! Meginiet velreiz" << std::endl << std::endl << std::endl;
+        std::cout << "Nepareizi!" << std::endl << "Pareiza atbilde: " << std::endl;
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                std::cout << MasivsAB[i][j] << "\t";
+            }
+            std::cout << std::endl;
+        }
     }
-    Sleep(2 * 1000);
+    std::cout << std::endl;
+    std::cout << "Piespiediet taustinu lai turpinatu...";
+    _getch();
     system("CLS");
 };
 
 
 void matricu_atnemsana() {
+    system("CLS");
+    std::cout << "MATRICAS ATNEMSANA" << std::endl << std::endl;
     // Pirmās matricas ievade
     int MasivsA[3][3];
     for (int i = 0; i < 3; i++)
@@ -124,31 +136,40 @@ void matricu_atnemsana() {
     {
         for (int j = 0; j < 3; j++)
         {
-            std::cout << "Ievadiet [" << i << "]" << "[" << j << "] skaitļu starpibu: ";
+            std::cout << "Ievadiet [" << i << "]" << "[" << j << "] skaitlu starpibu: ";
             std::cin >> Lietotajs[i][j];
             if (Lietotajs[i][j] == MasivsAB[i][j])
             {
                 pareizs += 1;
             }
             std::cout << std::endl;
-            std::cout << std::endl;
         }
     }
     if (pareizs == 9)
     {
-        std::cout << "Pareizi!";
+        std::cout << "Pareizi!" << std::endl;
     }
     else
     {
-        std::cout << "Nepareizi! Meginiet velreiz" << std::endl << std::endl << std::endl;
+        std::cout << "Nepareizi!" << std::endl << "Pareiza atbilde: " << std::endl;
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                std::cout << MasivsAB[i][j] << "\t";
+            }
+            std::cout << std::endl;
+        }
     }
-    Sleep(2 * 1000);
+    std::cout << std::endl;
+    std::cout << "Piespiediet taustinu lai turpinatu...";
+    _getch();
     system("CLS");
 };
 
 
 void matricu_reizinasana() {
-
+    system("CLS");
     // Pirmās matricas ievade
     int MasivsA[3][3];
     for (int i = 0; i < 3; i++)
@@ -216,73 +237,125 @@ void matricu_reizinasana() {
     }
     else
     {
-        std::cout << "Nepareizi! Meginiet velreiz" << std::endl << std::endl << std::endl;
+        std::cout << "Nepareizi!" << std::endl << "Pareiza atbilde: " << std::endl;
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                std::cout << MasivsAB[i][j] << "\t";
+            }
+            std::cout << std::endl;
+        }
     }
-    Sleep(2 * 1000);
+    std::cout << std::endl;
+    std::cout << "Piespiediet taustinu lai turpinatu...";
+    _getch();
+    system("CLS");
+};
+
+
+void determinants() {
+    system("CLS");
+    std::cout << "MATRICAS DETERMINANTA APREKINASANA" << std::endl << std::endl;
+    int MasivsA[3][3], i, j, lietotajs, det = 0;
+
+    // Matricas ievade
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            MasivsA[i][j] = rand() % 10;
+        }
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            std::cout << MasivsA[i][j] << "\t";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+
+    // Determinanta aprēķināšana
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            det = MasivsA[0][0] * ((MasivsA[1][1] * MasivsA[2][2]) - (MasivsA[2][1] * MasivsA[1][2]))
+                - MasivsA[0][1] * (MasivsA[1][0] * MasivsA[2][2] - MasivsA[2][0] * MasivsA[1][2])
+                + MasivsA[0][2] * (MasivsA[1][0] * MasivsA[2][1] - MasivsA[2][0] * MasivsA[1][1]);
+        }
+    }
+    std::cout << "Ievadiet dotas matricas determinantu: ";
+    std::cin >> lietotajs;
+    std::cout << std::endl;
+    if (lietotajs == det)
+    {
+        std::cout << "Pareizi!" << std::endl << std::endl;
+    }
+    else
+    {
+        std::cout << "Nepareizi!" << std::endl << "Pareiza atbilde: " << det << std::endl << std::endl;
+    }
+    std::cout << "Piespiediet taustinu lai turpinatu...";
+    _getch();
     system("CLS");
 };
 
 
 void pitagors() {
     system("CLS");
+    std::cout << "TRIJSTURA MALAS APREKINASANA" << std::endl << std::endl;
     int malas_izvade = rand() % 2 + 1;
+    int lietotajs;
+    int m = rand() % 10 + 1;
+    int n = rand() % 10 + 1;
+    int a = 0;
+    do {
+         m = rand() % 10 + 1;
+         n = rand() % 10 + 1;
+         a = pow(m, 2) - pow(n, 2);
+    } while (a <= 0);
+    int b = 2 * m * n;
+    int c = pow(m, 2) + pow(n, 2);
     if (malas_izvade == 1)
     {
-        int lietotajs;
-        int m = rand() % 10 + 1;
-        int n = rand() % 10 + 1;
-        int a = 0;
-        do {
-            m = rand() % 10 + 1;
-            n = rand() % 10 + 1;
-            a = pow(m, 2) - pow(n, 2);
-        } while (a <= 0);
-        int b = 2 * m * n;
-        int c = pow(m, 2) + pow(n, 2);
-        std::cout << "Pirma katetes mala ir " << a << " cm." << " Otra katetes mala ir " << b << " cm." << std::endl;
+        std::cout << "Pirma katetes mala ir " << a << " cm." << " Otra katetes mala ir " << b << " cm." << std::endl << std::endl;
         std::cout << "Aprekiniet hipotenuzas garumu!" << std::endl;
         std::cout << "Ievadiet hipotenuzas garumu: ";
         std::cin >> lietotajs;
         if (c == lietotajs)
         {
-            std::cout << std::endl << "Pareizi!";
+            std::cout << std::endl << "Pareizi!" << std::endl;
         }
         else
         {
-            std::cout << std::endl << "Nepareizi!";
+            std::cout << std::endl << "Nepareizi!" << std::endl << "Pareiza atbilde: " << c << std::endl;
         }
     }
     else
     {
-        int lietotajs;
-        int m = rand() % 10 + 1;
-        int n = rand() % 10 + 1;
-        int a = 0;
-        do {
-            m = rand() % 10 + 1;
-            n = rand() % 10 + 1;
-            a = pow(m, 2) - pow(n, 2);
-        } while (a <= 0);
-        int b = 2 * m * n;
-        int c = pow(m, 2) + pow(n, 2);
         std::cout << "Viena katetes mala ir " << a << " cm." << " Hipotenuza ir " << c << " cm." << std::endl << std::endl;
         std::cout << "Aprekiniet otras katetes garumu!" << std::endl;
         std::cout << "Ievadiet katetes garumu: ";
         std::cin >> lietotajs;
         if (b == lietotajs)
         {
-            std::cout << std::endl << "Pareizi!";
+            std::cout << std::endl << "Pareizi!" << std::endl;
         }
         else
         {
-            std::cout << std::endl << "Nepareizi!";
+            std::cout << std::endl << "Nepareizi!" << std::endl << "Pareiza atbilde: " << b << std::endl;
         }
     }
-    Sleep(2 * 1000);
+    std::cout << std::endl;
+    std::cout << "Piespiediet taustinu lai turpinatu...";
+    _getch();
     system("CLS");
 };
 
-
+//Kvadrātvienādojumi
 void kvadratvienadojumi() {
     system("CLS");
     int b, c, lietotajs, lietotajs2;
@@ -316,53 +389,161 @@ void kvadratvienadojumi() {
     if (x2 == lietotajs2)
         pareizs += 1;
 
+    std::cout << std::endl;
+
     if (pareizs == 2)
     {
-        std::cout << "Pareizi!";
+        std::cout << "Pareizi!" << std::endl;
     }
     else
-        std::cout << "Nepareizi!";
-    Sleep(2 * 1000);
+    {
+        std::cout << "Nepareizi!" << std::endl << "x1 = " << x1 << std::endl << "x2 = " << x2 << std::endl;
+    }
+    std::cout << std::endl;
+    std::cout << "Piespiediet taustinu lai turpinatu...";
+    _getch();
     system("CLS");
 };
 
+// Kombinatorika
+int faktorials(int skaitlis) {
+    int factorial = 1;
+    for (int i = 1; i <= skaitlis; ++i) {
+        factorial *= i;
+    }
+    return factorial;
+};
 
-int main() {
-    srand(time(0));
-    int tema;
-    std::cout << "Izvelieties matematikas temu, ievadot atbilstoso numuru!" << std::endl;
-    std::cout << "1. Matricas saskaitisana" << std::endl;
-    std::cout << "2. Matricas atnemsana" << std::endl;
-    std::cout << "3. Matricas reizinasana" << std::endl;
-    std::cout << "4. Trijstura malas aprekinasana" << std::endl;
-    std::cout << "5. Kvadratvienadojumi" << std::endl;
-    std::cout << std::endl << "Ievadiet temas numuru: ";
+void kombinatorika() {
+    system("CLS");
+    std::string pukes[6] = { "rozes", "tulpes", "gerberas",
+        "kallas", "begonijas", "lillijas" };
+    int RandIndex = rand() % 6;
+    int n, k, lietotajs;
+    n = rand() % 12 + 1;
+    k = 2;
+    do {
+        n = rand() % 12 + 1;
+    } while (n < 4);
+    std::cout << "KOMBINATORIKA" << std::endl << std::endl;
+    std::cout << "Veikala bija atrodamas " << n << " " << pukes[RandIndex] << "." << std::endl;
+    std::cout << "Cik veidos var izveleties " << k << " " << pukes[RandIndex] << "?" << std::endl;
+    int pirmaisSk = faktorials(n);
+    int otraisSk = 2 * faktorials(n - 2);
+    int rezultats = pirmaisSk / otraisSk;
+    std::cout << std::endl << "Ievadiet atbildi: ";
+    std::cin >> lietotajs;
+    std::cout << std::endl;
+    if (rezultats == lietotajs)
+        std::cout << "Pareizi!" << std::endl;
+    else
+        std::cout << "Nepareizi!" << std::endl << "Pareiza atbilde: " << rezultats << std::endl;
+    std::cout << std::endl;
+    std::cout << "Piespiediet taustinu lai turpinatu...";
+    _getch();
+    system("CLS");
+};
+
+void uzdevumi() {
+    system("CLS");
+    int tema = 0;
+    std::cout << "Izvelieties matematikas temu, ievadot atbilstoso numuru!" << std::endl << std::endl;
+    std::cout << " 1. Matricas Saskaitisana" << std::endl;
+    std::cout << " 2. Matricas Atnemsana" << std::endl;
+    std::cout << " 3. Matricas Reizinasana" << std::endl;
+    std::cout << " 4. Matricas Determinants" << std::endl;
+    std::cout << " 5. Trijstura Malas Aprekinasana" << std::endl;
+    std::cout << " 6. Kvadratvienadojumi" << std::endl;
+    std::cout << " 7. Kombinatorika" << std::endl;
+    std::cout << std::endl << " 8. Atpakal uz izvelni" << std::endl;
+    std::cout << std::endl << " Ievadiet temas numuru: ";
     std::cin >> tema;
     if (tema == 1)
     {
         matricu_saskaitisana();
-        return main();
+        return uzdevumi();
     }
     else if (tema == 2)
     {
         matricu_atnemsana();
-        return main();
+        return uzdevumi();
     }
     else if (tema == 3)
     {
         matricu_reizinasana();
-        return main();
+        return uzdevumi();
     }
     else if (tema == 4)
     {
-        pitagors();
-        return main();
+        determinants();
+        return uzdevumi();
     }
     else if (tema == 5)
     {
+        pitagors();
+        return uzdevumi();
+    }
+    else if (tema == 6)
+    {
         kvadratvienadojumi();
+        return uzdevumi();
+    }
+    else if (tema == 7)
+    {
+        kombinatorika();
+        return uzdevumi();
+    }
+    else if (tema == 8)
+        std::cout << " ";
+    else
+    {
+        std::cout << "Nepareizs numurs! Ludzu meginiet velreiz!";
+        Sleep(1 * 1000);
+        return uzdevumi();
+    }
+}
+
+
+void macibu_materials() {
+    system("CLS");
+    std::cout << "MACIBU MATERIALS" << std::endl << std::endl;
+    std::cout << " Matricas - https://www.uzdevumi.lv/p/matematika/1-kurss/matricas-1927/re-1050716f-640c-474d-a676-3b4fa6fbedc9";
+    std::cout << std::endl;
+    std::cout << " Pitagora teorema - https://lv.wikipedia.org/wiki/Pitagora_teor%C4%93ma";
+    std::cout << std::endl;
+    std::cout << " Kvadratvienadojumi - https://www.uzdevumi.lv/p/matematika/10-klase/racionalas-algebriskas-izteiksmes-993/kvadratvienadojums-un-kvadrattrinoms-12974/re-89b5a5e7-2221-436c-a463-71afbf4a40e6";
+    std::cout << std::endl;
+    std::cout << " Kombinatorika - https://www.uzdevumi.lv/p/matematika/11-klase/varbutibu-teorijas-elementi-2675/re-1339d1a2-66b2-46bb-9da0-2b3ffd02cd17";
+    std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << " Piespiediet taustinu lai turpinatu...";
+    _getch();
+}
+
+
+int main() {
+    system("CLS");
+    srand(time(0));
+    std::cout << " MATEMATIKAS UZDEVUMI\n\n 1. Uzdevumi\n 2. Macibu materials\n 3. Iziet" << std::endl << std::endl;
+    int izvele;
+    std::cout << " Ievadiet savu izveli: ";
+    std::cin >> izvele;
+    if (izvele == 1)
+    {
+        uzdevumi();
         return main();
     }
+    else if (izvele == 2)
+    {
+        macibu_materials();
+        return main();
+    }
+    else if (izvele == 3)
+    {
+        std::cout << " ";
+    }
+    else
+        return main();
 
     return 0;
 };
